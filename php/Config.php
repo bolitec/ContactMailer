19fecc0c1beb5817fc381204a6bd89d27a9007a4
lfbolivar <lfbolivar@bolitech.com>
2015-05-26 23:27:10 -0400

    Making config generic

--- php/Config.php
+++ php/Config.php
@@ -7,23 +7,23 @@
 # define('PATH', '/home/inaccessible_folder/images');
 # NOTE: there is no ending slash
 // define('PATH', str_replace('\\', '/', getcwd()));
-define('PRIVATE_SESSION', '/Users/Fernando/git/ContactMailer/ContactMailer/secure/');
-define('PRIVATE_DB', '/Users/Fernando/git/ContactMailer/ContactMailer/secure/ContactMailer/');
-define('Home_path', '/Users/Fernando/git/ContactMailer/');
-define('Home_url', 'http://localhost:8888/ContactMailer/');
-define ('Secure_path', '/Users/Fernando/git/ContactMailer/ContactMailer/secure/ContactMailer/');
+define('Home_path', '/Users/Fernando/Documents/2015/Projects');
+define('Home_url', 'http://localhost:8888');
+define('PRIVATE_SESSION', Home_path.'/ContactMailer/secure/');
+define('PRIVATE_DB', Home_path.'/ContactMailer/secure/ContactMailer/');
+define ('Secure_path', Home_path.'/ContactMailer/secure/ContactMailer/');
 
 // url locations
-$hdr_image = Home_url."ContactMailer/images/JCLMHeader.jpg";
-$url_images = Home_url."ContactMailer/images/";
-$url_secure_php = Home_url."ContactMailer/php/";
+$hdr_image = Home_url."/ContactMailer/images/JCLMHeader.jpg";
+$url_images = Home_url."/ContactMailer/images/";
+$url_secure_php = Home_url."/ContactMailer/php/";
 
 
 // set path for location of files on the server
-$html_files = Home_path."ContactMailer/html/";
-$php_files  = Home_path."ContactMailer/php/";
-$images_files  = Home_path."ContactMailer/images/";
-$mailer_files = Home_path."ContactMailer/mailer/";
+$html_files = Home_path."/ContactMailer/html/";
+$php_files  = Home_path."/ContactMailer/php/";
+$images_files  = Home_path."/ContactMailer/images/";
+$mailer_files = Home_path."/ContactMailer/mailer/";
 $secured_php = Secure_path;
 
 // reply email address on the regemail header
